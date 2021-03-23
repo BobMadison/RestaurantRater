462f00048d0d5eab46bb7998219b558803de4da3
Bob Madison <robertdavidmadison@gmail.com>
2021-03-22 22:57:52 -0400

    Edit Functionality Added

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -17,13 +17,13 @@ namespace RestaurantRater.Controllers
             return View(_db.Restaurants.ToList());
         }
 
-        // Get: Restaurant/Create
+        // GET: Restaurant/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // Post: Restaurant/Create
+        // POST: Restaurant/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Restaurant restaurant)
@@ -65,6 +65,40 @@ namespace RestaurantRater.Controllers
             return RedirectToAction("Index");
 
         }
+        // GET: Restaurant/Edit/{id}
+        // Get an id from the user
+        // Handle if the id is null
+        // Find a restaurant by that id
+        // If the restaurant doesn't exist or
+        public ActionResult Edit (int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound(); // or
+            //  return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+            return View(restaurant);
+        }
+        // POST: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
 
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
     }
 }
